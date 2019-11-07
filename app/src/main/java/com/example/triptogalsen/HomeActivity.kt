1e1b8d389db24a34ed5178c2664628c984a6c773
birame <birametgod95@gmail.com>
2019-11-07 11:48:11 +0100

    authentication (sign in , sign out , sign up) with firebase

--- app/src/main/java/com/example/triptogalsen/HomeActivity.kt
+++ app/src/main/java/com/example/triptogalsen/HomeActivity.kt
@@ -2,11 +2,32 @@ package com.example.triptogalsen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        signOut.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.signOut -> signOut()
+        }
+    }
+
+     fun signOut(){
+        Log.i("CLICK","DECONNECTER")
+        FirebaseAuth.getInstance().signOut()
+        val myIntent = SplashActivity.openConnexionActivity(this)
+        startActivity(myIntent)
+        finish()
     }
 }
